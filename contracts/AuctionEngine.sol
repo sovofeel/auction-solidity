13ec195acb58f4957910b614321a039d502a751f
sovofeel <sovofeel@gmail.com>
2022-04-23 15:11:33 +0500

    define sinitial variables

--- contracts/AuctionEngine.sol
+++ contracts/AuctionEngine.sol
@@ -1,4 +1,20 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-contract AuctionEngine {}
+contract AuctionEngine {
+    // Владелец площадки
+    address public owner;
+    uint256 constant DURATION = 2 days;
+    uint256 constant FEE = 12; // Комиссия 12%
+
+    struct Auction {
+        address payable seller;
+        uint256 startingPrice;
+        uint256 finalPrice;
+        uint256 startAt;
+        uint256 endAt;
+        uint256 discountRate;
+        string item;
+        bool stopped;
+    }
+}
