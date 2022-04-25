c958a595e252a8b1888e2850c217c95350253122
sovofeel <sovofeel@gmail.com>
2022-04-25 17:29:30 +0500

    add test create auction

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const AuctionEngine= await hre.ethers.getContractFactory("AuctionEngine");
+  const auctionEngine = await AuctionEngine.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  await auctionEngine.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("AuctionEngine deployed to:", auctionEngine.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- test/AuctionEngine-test.js
+++ test/AuctionEngine-test.js
@@ -1,7 +1,7 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("AuctionEngine", function () {
+describe("AuctionEngine", () => {
     let owner
     let seller
     let buyer
@@ -15,8 +15,23 @@ describe("AuctionEngine", function () {
       await auction.deployed();
     })
 
-    it("sets owner", async () => {
-      const currentOwner = await auction.owner();
+    it("> sets correct owner", async () => {
+      const currentOwner = await auction.owner(); 
       expect(currentOwner).to.eq(owner.address)
+    })
+
+    describe("createAuction", () => {
+      it("> creates auction correctly", async () => {
+        const tx = await auction.createAuction(
+          ethers.utils.parseEther("0.0001"),
+          3,
+          "mock item",
+          60
+        )
+
+        const currentAuction =  await auction.auctions(0)
+        expect(currentAuction.item).to.eq("mock item")
+         
+      });
     })
 });
